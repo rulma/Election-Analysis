e294e346fb342238efebbdc3586b969cf660a06c
Mateo Rul <mateoarul@gmail.com>
2021-05-03 20:28:53 -0500

    cleared excess comments

--- AuditScript.py
+++ AuditScript.py
@@ -1,38 +1,3 @@
-# one = 5 + 2 * 3
-# two = 8 // 5 - 3
-# three =8 + 22 * 2 - 4
-# four = 16 - 3 / 2 + 7 - 1
-# five = 3 ** 3 % 5
-# six = 5 + 9 * 3 / 2 - 4
-
-# print(one)
-# print(two)
-# print(three)
-# print(four)
-# print(five)
-# print(six)
-
-# counties = ["Fairfax","Santa Cruz", "Hermosa"]
-# print(counties)
-# user_county = ""
-
-# user_county = input("What County do you live in? ")
-# counties.append(user_county+"ab")
-
-# print(counties)
-
-# user_fix = input("Oh no I mispelled your county. Can you reenter it for me? ")
-# counties[3] = user_fix
-# print(counties)
-
-# my_dictionary = {}
-# user_word = input("Give me a word: ")
-
-# user_definition = input("Now define that word: ")
-# my_dictionary[user_word] = user_definition
-# print(my_dictionary)
-# print(my_dictionary.keys())
-
 #create empty voting data dictionary
 voting_data = []
 
